f791abb3e327026c9109271bdfaec82cb44394d6
Marlon <marlon62009@gmail.com>
2020-05-21 09:13:34 -0500

    Previo (Organizar carpeta)

---
+++ PHP/VALIDARINGRESO.php
@@ -0,0 +1,13 @@
+<?PHP
+$Nombre = $_POST["nombre"];
+$Papellido = $_POST["apellido1"];
+$Sapellido = $_POST["apellido2"];
+$Correo = $_POST["correo"];
+$Contrasena = $_POST["contrasena"];
+$Numero = $_POST["telefono"];
+
+
+
+
+
+?>
---
+++ PHP/VALIDARREGISTRO.php
@@ -0,0 +1,17 @@
+<?php
+$Nombre = $_POST["nombre"];
+$Papellido = $_POST["apellido1"];
+$Sapellido = $_POST["apellido2"];
+$Correo = $_POST["correo"];
+$Contrasena = $_POST["contrasena"];
+$Numero = $_POST["telefono"];
+$a = 9;
+
+echo $a;
+echo $_POST["nombre"];
+echo apellido1;
+echo apellido2;
+echo correo;
+echo telefono;
+echo contrasena;
+?>
