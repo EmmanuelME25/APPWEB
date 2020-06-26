e47a0eff4e54e073257844d50848b791684920a4
Marlon <marlon62009@gmail.com>
2020-06-25 19:27:17 -0500

    Bug and style fix 1.0

--- PRINCIPAL/PRESTAMOS/index.php
+++ PRINCIPAL/PRESTAMOS/index.php
@@ -15,16 +15,11 @@
 
 ?>
 <html>
-
-    
-
-    <html>
     <head>
         <link rel="stylesheet" type="text/css" href="../../CSS/pages.css" media="screen"/>
         <title>Préstamos</title>
     </head>
     <body>
-		<div class='Cuadrado'></div>
 		<div id="left">
 			<h1 class="1">En proceso de aceptación</h1><hr>
 			<table>
@@ -119,52 +114,55 @@
 					$stmt->execute();
 					$LibroP = $stmt->get_result();
 					$LibroP = $LibroP->fetch_object();
-					$YC = $LibroP->COMENTPREST;
 					
-					//Si el libro está en solicitud
-					if($LibroP->ESTADOPR=='PRESTADO' || $LibroP->ESTADOPR=='DEVUELTO'){
+					if($LibroP){
+					
+						$YC = $LibroP->COMENTPREST;
 						
-						$PEs = $LibroP->ESTADOPR;
-						
-						//Obtener ID del usuario que solicita el libro
-						$stmt = $conn->prepare("SELECT * FROM prestamo WHERE ID_L=?");
-						$stmt->bind_param("s",$IDL);
-						$stmt->execute();
-						$IDUP = $stmt->get_result();
-						$IDUP = $IDUP->fetch_object();
-						$IDUP = $IDUP->ID_U;
-						
-						//Obtener información del usuario que solicita el libro
-						$stmt = $conn->prepare("SELECT * FROM usuarios WHERE ID_U=?");
-						$stmt->bind_param("s",$IDUP);
-						$stmt->execute();
-						$UP = $stmt->get_result();
-						$UP = $UP->fetch_object();
+						//Si el libro está en solicitud
+						if($LibroP->ESTADOPR=='PRESTADO' || $LibroP->ESTADOPR=='DEVUELTO'){
+							
+							$PEs = $LibroP->ESTADOPR;
+							
+							//Obtener ID del usuario que solicita el libro
+							$stmt = $conn->prepare("SELECT * FROM prestamo WHERE ID_L=?");
+							$stmt->bind_param("s",$IDL);
+							$stmt->execute();
+							$IDUP = $stmt->get_result();
+							$IDUP = $IDUP->fetch_object();
+							$IDUP = $IDUP->ID_U;
+							
+							//Obtener información del usuario que solicita el libro
+							$stmt = $conn->prepare("SELECT * FROM usuarios WHERE ID_U=?");
+							$stmt->bind_param("s",$IDUP);
+							$stmt->execute();
+							$UP = $stmt->get_result();
+							$UP = $UP->fetch_object();
 
-						$Imagen = $Libro->BookPic;
-						$Titulo = $Libro->TITULO;
-						$Autor = $Libro->AUTOR;
-						$NombreUP = $UP->NOMBRE;
-						$AP1UP = $UP->APELLIDO1;
-						$AP2UP = $UP->APELLIDO2;
-				
-							echo "  
-											<tr><td><div style='padding: 10; align-content: center;'>
-												<img src=$Imagen height=240 width=135 style='object-fit: cover'>
-											</div></td><td><div style='padding: 10; align-content: center;'>
-												$Titulo
-												<br>
-												Autor: $Autor
-												<br><br>
-												Solicitado por: $NombreUP $AP1UP $AP2UP
-												<br><br>
-												$PEs
-											</div></td><td><div style='padding: 10; align-content: center;'>
-												Comentarios del usuario: <br><i> $YC </i>
-											</div></td>
-										";
-						
-				}
+							$Imagen = $Libro->BookPic;
+							$Titulo = $Libro->TITULO;
+							$Autor = $Libro->AUTOR;
+							$NombreUP = $UP->NOMBRE;
+							$AP1UP = $UP->APELLIDO1;
+							$AP2UP = $UP->APELLIDO2;
+					
+								echo "  
+												<tr><td><div style='padding: 10; align-content: center;'>
+													<img src=$Imagen height=240 width=135 style='object-fit: cover'>
+												</div></td><td><div style='padding: 10; align-content: center;'>
+													$Titulo
+													<br>
+													Autor: $Autor
+													<br><br>
+													Solicitado por: $NombreUP $AP1UP $AP2UP
+													<br><br>
+													$PEs
+												</div></td><td><div style='padding: 10; align-content: center;'>
+													Comentarios del usuario: <br><i> $YC </i>
+												</div></td>
+											";
+							}
+						}
 				echo "</tr>";
 			}
 			?>
@@ -172,8 +170,7 @@
 			</table>
         </div>
 		<div class='Cuadrado'></div>
-	<div class='Cuadrado1'></div>
+		<div class='Cuadrado1'></div>
     </body>
-    </html>
     <a href="../" target="_self"><img src="../../IMAGENES/logo.png" class="logoprinc" alt="Logo" width=50 height=50></a>
 </html>
